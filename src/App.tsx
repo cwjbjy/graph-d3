90b9319b6a867b021b7e6fb7e1b7e15972eb31fb
cwjbjy <595842570@qq.com>
2023-12-20 15:15:19 +0800

    feat: d3画图谱

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Graph from "./graph";
+import "antd/dist/reset.css";
+
+function App() {
+  return <Graph />;
+}
+
+export default App;
---
+++ src/graph/index.tsx
@@ -0,0 +1,11 @@
+import MindGraph from "./components/mindGraph";
+
+const Login = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <MindGraph width="100%" height="calc(100vh - 60px)" />
+    </div>
+  );
+};
+
+export default Login;
